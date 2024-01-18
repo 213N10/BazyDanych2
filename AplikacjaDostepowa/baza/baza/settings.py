7cefa50adca599829e2108bb62248df44377ec88
Janek <75185339+213N10@users.noreply.github.com>
2024-01-18 14:00:58 +0100

    sprzatanie

--- AplikacjaDostepowa/baza/baza/settings.py
+++ AplikacjaDostepowa/baza/baza/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'administracjarp',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '1101',
         'HOST':'localhost',
         'PORT':'3306',
     }
